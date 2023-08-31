26f72b377d6d19ac6222d83558af556b507258d8
Rosimary Privado <136392896+maryprivado@users.noreply.github.com>
2023-08-31 03:18:04 -0300

    projeto

---
+++ perfil/views.py
@@ -0,0 +1,93 @@
+from datetime import datetime
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from extrato.models import Valores
+from perfil.models import Conta, Categoria
+from django.contrib.messages import constants
+from .utils import calcula_equilibrio_financeiro, calcula_total
+from django.db.models import Sum
+
+# Create your views here.
+def home(request):
+    valores = Valores.objects.filter(data__month=datetime.now().month)
+    entradas = valores.filter(tipo='E')
+    saidas = valores.filter(tipo='S')
+
+    total_entradas = calcula_total(entradas, 'valor')
+    total_saidas = calcula_total(saidas, 'valor')
+    contas = Conta.objects.all()
+    saldo_total = calcula_total(contas, 'valor')
+    percentual_gastos_essenciais, percentual_gastos_nao_essenciais = calcula_equilibrio_financeiro()
+    return render(request, 'home.html', {'contas': contas, 'saldo_total': saldo_total,})
+
+def gerenciar(request):
+    contas = Conta.objects.all()
+    categorias = Categoria.objects.all()
+    
+    saldo_total = calcula_total(contas, 'valor')
+
+    
+    return render(request, 'gerenciar.html', {'contas': contas, 'saldo_total': saldo_total, 'categorias': categorias})
+
+def cadastrar_banco(request):
+    apelido = request.POST.get('apelido')
+    banco = request.POST.get('banco')
+    tipo = request.POST.get('tipo')
+    valor = request.POST.get('valor')
+    icone = request.FILES.get('icone')
+    
+    if len(apelido.strip()) == 0 or len(valor.strip()) == 0:
+        messages.add_message(request, constants.ERROR, 'Preencha todos os campos')
+        return redirect('/perfil/gerenciar/')
+    
+    conta = Conta(
+        apelido = apelido,
+        banco=banco,
+        tipo=tipo,
+        valor=valor,
+        icone=icone
+    )
+
+    conta.save()
+    messages.add_message(request, constants.SUCCESS, 'Cadastro feito com sucesso!')
+
+    return redirect('/perfil/gerenciar/')
+
+def deletar_banco(request, id):
+    conta = Conta.objects.get(id=id)
+    conta.delete()
+    
+    messages.add_message(request, constants.SUCCESS, 'Conta removida com sucesso')
+    return redirect('/perfil/gerenciar/')
+
+def cadastrar_categoria(request):
+    nome = request.POST.get('categoria')
+    essencial = bool(request.POST.get('essencial'))
+
+    categoria = Categoria(
+        categoria=nome,
+        essencial=essencial
+    )
+
+    categoria.save()
+
+    messages.add_message(request, constants.SUCCESS, 'Categoria cadastrada com sucesso')
+    return redirect('/perfil/gerenciar/')
+
+def update_categoria(request, id):
+    categoria = Categoria.objects.get(id=id)
+
+    categoria.essencial = not categoria.essencial
+
+    categoria.save()
+
+    return redirect('/perfil/gerenciar/')
+
+def dashboard(request):
+    dados = {}
+    categorias = Categoria.objects.all()
+
+    for categoria in categorias:
+        dados[categoria.categoria] = Valores.objects.filter(categoria=categoria).aggregate(Sum('valor'))['valor__sum']
+
+    return render(request, 'dashboard.html', {'labels': list(dados.keys()), 'values': list(dados.values())})
